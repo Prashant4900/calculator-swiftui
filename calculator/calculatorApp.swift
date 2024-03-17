9047ad50589c8a18ed2a8f758739d9f0350d71c0
Prashant Nigam <62440911+Prashant4900@users.noreply.github.com>
2024-03-17 14:52:15 +0530

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Prashant Nigam on 17/03/24.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
